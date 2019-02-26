e32d4a4d04d888014976ead33ff4c7a6d9b4cabc
sarahkittyy <chrisohlin1@gmail.com>
2019-02-26 15:46:03 -0500

    added python's filter()

---
+++ include/PythoniC/Util/Filter.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace py
+{
+
+/**
+ * @brief A recreation of python's filter() function. Takes in a container and a predicate and returns a container with only elements that the predicate returns true.
+ * 
+ * @tparam IterableContainer An STL container with, at the minimum, begin(), end(), and insert(). 
+ * @tparam Predicate A function that takes 1 parameter and returns bool.
+ * 
+ * @param pred The predicate of which to test each value on.
+ * @param container The container to iterate over.
+ * 
+ * @return IterableContainer The filtered container.
+ */
+template <class IterableContainer, typename Predicate>
+IterableContainer filter(Predicate pred, IterableContainer container)
+{
+	//Create the return container.
+	IterableContainer filtered;
+	//Iterate over each item.
+	for (auto &item : container)
+	{
+		//If the predicate returns true...
+		if (pred(item))
+		{
+			//This is the most generic way I could find of
+			//appending to the back of a container.
+			filtered.insert(filtered.end(), item);
+		}
+	}
+
+	return filtered;
+}
+
+}
---
+++ src/Util/Filter.cpp
@@ -0,0 +1,1 @@
+#include "PythoniC/Util/Filter.hpp"
