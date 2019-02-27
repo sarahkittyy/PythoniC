abef47f8dbad7a415bf4c27d012a82364d3ef323
sarahkittyy <chrisohlin1@gmail.com>
2019-02-26 20:43:12 -0500

    fixed compiling sources

---
+++ src/PythoniC.cpp
@@ -0,0 +1,4 @@
+#include "PythoniC/PythoniC.hpp"
+
+//This source is completely unnecessary, but cmake whines at me if I don't have it and I'd like to keep this a linkable library for future compatibility.
+//Thanks, C++, for making template classes so difficult ;-;
